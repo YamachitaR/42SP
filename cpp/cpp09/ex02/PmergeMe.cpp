98121f9245e15941a4fc44463c747ef68d2d6ba6
rick <rickyamas@gmail.com>
2023-10-28 23:04:26 -0300

    os primeiros passdo do cpp09/ex02

--- cpp/cpp09/ex02/PmergeMe.cpp
+++ cpp/cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,61 @@
 /*   By: ryoshio- <ryoshio-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 07:12:55 by ryoshio-          #+#    #+#             */
-/*   Updated: 2023/10/28 07:17:29 by ryoshio-         ###   ########.fr       */
+/*   Updated: 2023/10/28 23:03:15 by ryoshio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe(void){ 
+    std::cout << "PmergeMe: Default constructor called" << std::endl;
+}
+
+PmergeMe::PmergeMe(PmergeMe const & copy)
+{
+    std::cout << "PmergeMe: Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+PmergeMe::~PmergeMe(void){ 
+    std::cout << "PmergeMe: Destructor called" << std::endl;
+}
+
+
+PmergeMe &PmergeMe::operator=(PmergeMe const &rhs)
+{
+    std::cout << "PmergeMe : Copy assignment operator" << std::endl;
+	if ( this != &rhs )
+	{
+	}
+	return *this;
+}
+
+void PmergeMe::sortVector(void){ // tem que fazer tramento aqui dentro 
+    std::vector<int> vector = {1, 2, 3, 4, 5};
+    
+    int i;
+    
+    // forma o par 
+    i = 0;
+    while(i < vector.size() - 1){
+        if(vector.size() % 2 == 1  && i >= vector.size() - 2)
+            break;
+        
+        
+    }
+    
+
+    
+} 
+
+
+
+int  jacobsthal(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (jacobsthal(n - 1) + 2 * jacobsthal(n - 2));
+}
--- cpp/cpp09/ex02/PmergeMe.hpp
+++ cpp/cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,36 @@
 /*   By: ryoshio- <ryoshio-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 07:13:01 by ryoshio-          #+#    #+#             */
-/*   Updated: 2023/10/28 07:17:42 by ryoshio-         ###   ########.fr       */
+/*   Updated: 2023/10/28 22:39:41 by ryoshio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <cstring>
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <sys/time.h>
+#include <iomanip>
+
+class PmergeMe
+{
+	public:
+		PmergeMe(void);
+		PmergeMe(PmergeMe const &copy);
+		~PmergeMe(void);
+
+		PmergeMe &operator=(PmergeMe const &rhs);
+
+        void sortVector(void); //fazer o tratamento dentro 
+
+
+};
+
+
+int  jacobsthal(int n);
+
+#endif

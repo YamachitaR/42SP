26a369ce87d2f453c911472081ad88f0431da020
rick <rickyamas@gmail.com>
2023-10-29 05:27:47 -0300

    cpp09/ex02: falta pouco

--- cpp/cpp09/ex02/PmergeMe.cpp
+++ cpp/cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: ryoshio- <ryoshio-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 07:12:55 by ryoshio-          #+#    #+#             */
-/*   Updated: 2023/10/29 00:36:31 by ryoshio-         ###   ########.fr       */
+/*   Updated: 2023/10/29 05:27:09 by ryoshio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,25 +38,58 @@ PmergeMe &PmergeMe::operator=(PmergeMe const &rhs)
 
 void PmergeMe::sortVector(void){ // tem que fazer tramento aqui dentro 
     std::vector<int> v;
+    std::vector<int> vchain;
+    std::vector<int> vpend;
+    std::vector<int> index;
+    int n, i, j, value;
 
-    v.push_back(5);
-    v.push_back(4);
-    v.push_back(3);
-    v.push_back(2);
-    v.push_back(1);
+    i = 3000;
+    while( i > 0){
+        
+        v.push_back(i);
+        i--;
+    }
     
     iter(v.data(), v.size(), printElement<int>);
     std::cout << std::endl;
     sortPair(v);
-     
-    iter(v.data(), v.size(), printElement<int>);
-    std::cout << std::endl;
+    
+    
+ 
+
+   
+    
+    createChainPend(v, vchain, vpend);
+    
+
+    sortChain(vchain, vpend);
+    
+
+
+    n = vpend.size();
+    index = _indexJacobsthal(n);
+    
+    i = 0;
+    while(i < n){
+        value = vpend[index[i] - 1];
+        j = 0;
+        while(vchain[j] < value && vchain[j])
+            j++;
+        vchain.insert(vchain.begin() + j, value);
+        i ++;
+    }
+    
+    std::cout <<"|" <<std::endl;
+     iter(vchain.data(), vchain.size(), printElement<int>);
+      std::cout <<"|" <<std::endl;
 } 
 
 
 void PmergeMe::sortDeque(void){ // tem que fazer tramento aqui dentro 
     std::deque<int> d;
-
+    std::deque<int> dchain;
+    std::deque<int> dpend;
+    
     d.push_back(5);
     d.push_back(4);
     d.push_back(3);
@@ -65,15 +98,52 @@ void PmergeMe::sortDeque(void){ // tem que fazer tramento aqui dentro
     
     sortPair(d);
 
-    
+    createChainPend(d, dchain, dpend);
+
+    iter(d, d.size(), printElement<int>);
+
+     std::cout << std::endl;
+     iter(dchain, dchain.size(), printElement<int>);
+    std::cout << std::endl;
+     iter(dpend, dpend.size(), printElement<int>);
+
+    sortChain(dchain, dpend);
+
+    std::cout << std::endl;
+     iter(dchain, dchain.size(), printElement<int>);
+    std::cout << std::endl;
+     iter(dpend, dpend.size(), printElement<int>);
+
+  
 } 
 
 
 
+std::vector<int> PmergeMe::_indexJacobsthal(size_t n){
+    std::vector<int> index;
+    int i = 0;
+    int jaco1;
+    int jaco2;
+    
+    while( index.size() < n){
+        jaco1 = jacobsthal(i);
+        jaco2 = jacobsthal(i + 1);
+        
+        while(jaco2  > jaco1){
+            if( jaco2 <= (int) n)
+                index.push_back(jaco2);
+                
+            jaco2 --;
+        }
+        i ++;
+    }
+    return(index);
+}
 
-int  jacobsthal(int n)
-{
-	if (n == 0)
+
+
+int jacobsthal(int n) {
+ 	if (n == 0)
 		return (0);
 	if (n == 1)
 		return (1);
--- cpp/cpp09/ex02/PmergeMe.hpp
+++ cpp/cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ryoshio- <ryoshio-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 07:13:01 by ryoshio-          #+#    #+#             */
-/*   Updated: 2023/10/29 00:36:07 by ryoshio-         ###   ########.fr       */
+/*   Updated: 2023/10/29 04:05:05 by ryoshio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,10 @@
 
 class PmergeMe
 {
+    private:
+    
+        std::vector<int> _indexJacobsthal(size_t n);
+        
 	public:
 		PmergeMe(void);
 		PmergeMe(PmergeMe const &copy);
@@ -67,6 +71,17 @@ void iter(T *array,  size_t n,  void (*f)(T) )
 }
 
 template <typename T>
+void iter(const std::deque<T>& deq,  size_t n, void (*f)(T)) {
+
+    size_t i = 0;
+    while(i < n) {
+        f(deq[i]);
+        i++;
+    }
+}
+
+
+template <typename T>
 void printElement(T element) {
     std::cout << element << " ";
 }
@@ -82,6 +97,47 @@ void sortPair(T &a) {
         i+=2;
     }
 }
+
+template <typename T>
+void createChainPend(T &a, T &chain, T &pend){
+    size_t i = 1;
+    
+    while(i < a.size()){
+        chain.push_back(a[i-1]);
+        pend.push_back(a[i]);
+        i += 2;
+    } 
+    if(a.size() % 2 ==1)
+        pend.push_back(a[i -  1]);
+}
+
+template <typename T>
+bool isSort(T &a){
+    size_t i = 1;
+    while(i < a.size()) {
+        if(a[i-1] > a[i])
+            return(false);
+        i++;
+    }
+    return(true);
+}
+
+template <typename T>
+void sortChain(T &chain, T &pend){
+    size_t i = 1;
+    while(!isSort(chain)) {
+        if(i == chain.size())
+            i = 1;
+        if(chain[i-1] > chain[i])
+        {
+           swap(chain[i-1], chain[i]);   
+           swap(pend[i-1], pend[i]); 
+        }      
+        i++;
+    }
+}
+
+
 
 
 #endif
